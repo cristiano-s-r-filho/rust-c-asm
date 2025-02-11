a65b36ba679e6be6e6ed5cc9d15dabf83aa678f0
Pedro Antonio <pedroarmc.pr@gmail.com>
2025-02-10 22:54:21 -0300

    funcoes moves

---
+++ asm_cli/target/debug/deps/asm_cli-cb5411b37c646fce.d
@@ -0,0 +1,5 @@
+C:\Users\pedro\VS Code\ArqComp\rust-c-asm\asm_cli\target\debug\deps\libasm_cli-cb5411b37c646fce.rmeta: src\main.rs
+
+C:\Users\pedro\VS Code\ArqComp\rust-c-asm\asm_cli\target\debug\deps\asm_cli-cb5411b37c646fce.d: src\main.rs
+
+src\main.rs:
--- asm_cli/target/debug/deps/colored-43c8dee68ac29b81.d
+++ asm_cli/target/debug/deps/colored-43c8dee68ac29b81.d
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-C:\Users\Thales\Documents\.UFBA\Graduação\24.2\Arquitetura de Computadores\trabalho\rust-c-asm\asm_cli\target\debug\deps\libcolored-43c8dee68ac29b81.rmeta: C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\lib.rs C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\color.rs C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\control.rs C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\error.rs C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\style.rs C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\customcolors.rs
-
-C:\Users\Thales\Documents\.UFBA\Graduação\24.2\Arquitetura de Computadores\trabalho\rust-c-asm\asm_cli\target\debug\deps\colored-43c8dee68ac29b81.d: C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\lib.rs C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\color.rs C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\control.rs C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\error.rs C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\style.rs C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\customcolors.rs
-
-C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\lib.rs:
-C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\color.rs:
-C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\control.rs:
-C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\error.rs:
-C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\style.rs:
-C:\Users\Thales\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\customcolors.rs:
-=======
 C:\Users\pedro\VS Code\ArqComp\rust-c-asm\asm_cli\target\debug\deps\libcolored-43c8dee68ac29b81.rmeta: C:\Users\pedro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\lib.rs C:\Users\pedro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\color.rs C:\Users\pedro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\control.rs C:\Users\pedro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\error.rs C:\Users\pedro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\style.rs C:\Users\pedro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\customcolors.rs
 
 C:\Users\pedro\VS Code\ArqComp\rust-c-asm\asm_cli\target\debug\deps\colored-43c8dee68ac29b81.d: C:\Users\pedro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\lib.rs C:\Users\pedro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\color.rs C:\Users\pedro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\control.rs C:\Users\pedro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\error.rs C:\Users\pedro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\style.rs C:\Users\pedro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\customcolors.rs
@@ -20,4 +8,3 @@ C:\Users\pedro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.
 C:\Users\pedro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\error.rs:
 C:\Users\pedro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\style.rs:
 C:\Users\pedro\.cargo\registry\src\index.crates.io-6f17d22bba15001f\colored-3.0.0\src\customcolors.rs:
->>>>>>> 03e0402680ba8cafa7f9e58e6035e3e1d27d987c
